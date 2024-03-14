f9da4f8547aa9f2d9de0f8a866c27750ab77b5fa
Anastasia <3778242nastya@gmail.com>
2024-03-14 23:57:20 +0300

    in the function MyBox posted 1,2,3 stairs , add button

--- app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
@@ -36,6 +36,7 @@ fun NavigationScreen() {
             NavigationButton(stringResource(id = R.string.button_alertDialog), Screen.AlertDialog)
             NavigationButton(stringResource(id = R.string.button_row_screen), Screen.Row)
             NavigationButton(stringResource(id = R.string.button_column_screen), Screen.Column)
+            NavigationButton(stringResource(id = R.string.button_box_screen), Screen.Box)
 
         }
     }
